ff9ef607a6e7e6847a4e394c07a6fc9a4e3c24f9
maksym <maxym1998@gmail.com>
2024-05-28 12:08:23 +0300

    Fixed security group outbound

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -47,20 +47,30 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    ipv6_cidr_blocks  = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    ipv6_cidr_blocks  = ["::/0"]
   }
 }
 
@@ -69,22 +79,30 @@ resource "aws_security_group" "all_worker_mgmt" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
-    ]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    ipv6_cidr_blocks  = ["::/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0", "::/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    ipv6_cidr_blocks  = ["::/0"]
   }
 }
 
